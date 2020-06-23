190cacee97fa0ee9dde3b4707aa76ab066190e0f
hasecom <hasetennisth@gmail.com>
2020-06-23 23:02:14 +0900

    update

--- db.js
+++ db.js
@@ -28,9 +28,9 @@ MongoClient.connect(url, connectOption, (err, client) => {
 
   /* 接続に成功すればコンソールに表示 */
   console.log('Connected successfully to server');
-
+  const db = client.db(dbName);
+  
   /** DBに挿入する処理 users->collection (RDBだとtable) */
-  // const db = client.db(dbName);
   // db.collection('users',function(err,collection){
   //   collection.insertMany([
   //     {name:'hase', age: '23'},
@@ -39,6 +39,19 @@ MongoClient.connect(url, connectOption, (err, client) => {
   //     client.close();
   //   })
   // });
+
+  /* nameがitoのドキュメント(レコード)を削除 */
+  // db.collection('users').remove({
+  //   name:'ito'
+  // });
+
+  /* nameがhaseのドキュメントを23歳に変更 (nameも変更可能) */
+  //※ちなみに一意でない値でupdateを加える場合、複数件のうち一番上の値が変更される
+  //※upsert:trueの場合、変更がない場合はそのまま受容される?
+  // db.collection('users').update(
+  //   { name:'hase' },{ name:'hase', age:'23' }, { upsert:true }
+  // );
+
   /*DBから値を取得*/
   db.collection('users').find({}).toArray(function(err,result){
     if(err) throw err;
